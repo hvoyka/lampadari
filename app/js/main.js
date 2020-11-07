71401e17a593724cd62e25412acdbb340cacb90b
Lavrentev Stanislav <hvoinii@gmail.com>
2020-11-07 19:48:43 +0600

    add modal html, styles, js

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,20 @@
 $(function () {
+  //MAIN SLIDER
   $('.main__slider').slick({
     infinite: true,
     speed: 300,
     slidesToShow: 1,
     dots: true,
     arrows: false,
+  });
+
+  //MODAL
+  $("#open-modal-btn").on('click', function () {
+    $("#modal").fadeIn();
+    $('body').addClass('no-scroll');
+  });
+  $("#modal__close-btn, #modal__bg").on('click', function () {
+    $("#modal").fadeOut();
+    $('body').removeClass('no-scroll');
   });
 });
