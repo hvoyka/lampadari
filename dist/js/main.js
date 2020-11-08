e22f45c15946544b8d8ffd11769e8f3a5bec73f3
Lavrentev Stanislav <hvoinii@gmail.com>
2020-11-08 12:45:47 +0600

    first build

---
+++ dist/js/main.js
@@ -0,0 +1,32 @@
+$(function () {
+  //HEADER BURGER MENU
+  $("#header__burg").on('click', function () {
+    $("#nav-list").slideToggle();
+  });
+  //MAIN SLIDER
+  $('.main__slider').slick({
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    dots: true,
+    arrows: false,
+    appendDots: $('.main__slider-dots'),
+  });
+
+  //APERTI
+
+  $("#aperti__btn").on('click', function () {
+    $("#aperti__wrapper").slideToggle();
+    $("#aperti__btn").toggleClass('aperti__btn--open');
+  });
+
+  //MODAL
+  $("#open-modal-btn").on('click', function () {
+    $("#modal").fadeIn();
+    $('body').addClass('no-scroll');
+  });
+  $("#modal__close-btn, #modal__bg").on('click', function () {
+    $("#modal").fadeOut();
+    $('body').removeClass('no-scroll');
+  });
+});
